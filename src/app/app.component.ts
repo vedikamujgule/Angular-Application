299e1112cbfb906f4ca7d71b2e5900189048a781
Vedika Mujgule <vedikamujgule@gmail.com>
2023-03-25 07:53:00 -0400

    [update] forms module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  name: string = '';
+  @Output()
+  search: EventEmitter<String> = new EventEmitter<String>();
+  ngOnInit(): void {
+  }
+  
+onSearch(){
+  this.search.emit(this.name)
+}
+onSearchEnter(search:string){
+  this.search.emit(this.name);
+}
+transferData(){
+  console.log(`Name: ${this.name}`);
+}
+  data = ['Mad','MJ', 'Sanu','PreetyP']
   message = 'Angular-Application';
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,27 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './container/container.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { DemoComponent } from './demo/demo.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContainerComponent,
-    NavigationComponent
+    NavigationComponent,
+    DemoComponent,
+    CustomerListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
